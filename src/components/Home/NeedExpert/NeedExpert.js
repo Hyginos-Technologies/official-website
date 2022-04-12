624cc0dbea49d88aa8df9a5191daa5c8fb426206
gimnakatugampala <gimnakatugampala1@gmail.com>
2022-04-12 20:21:25 +0530

    our team section

--- src/components/Home/NeedExpert/NeedExpert.js
+++ src/components/Home/NeedExpert/NeedExpert.js
@@ -4,6 +4,7 @@ import './NeedExpert.css'
 import E1 from '../../../img/e-1.png'
 import E2 from '../../../img/e-2.png'
 import E3 from '../../../img/e-3.png'
+import E4 from '../../../img/e-4.png'
 import B1 from '../../../img/group-b.png'
 
 const NeedExpert = () => {
@@ -15,30 +16,67 @@ const NeedExpert = () => {
             </div>
 
         <div className='row'>
-            <div className='col-md-4 text-center expert-image-left'>
+            <div className='col-md-4 text-center expert-image-left expert-image-right'>
                 <img src={B1} className='box-left' />
-                <img src={E1} alt='e' />
+                <img src={E1} alt='e' className='e-image' />
                 <div className='expert-content'>
-                <h5>MADAM QUERY</h5>
+                <h5>GAVARRAJU RAJAPANTHULA</h5>
                 <p>CEO/IT INDUSTRY</p>
                 </div>
+
+                  {/* Social Media */}
+                  <div className='social-single-link'>
+                    <a href='#'><i class="fa-brands fa-twitter"></i></a>
+                    <a href='#'><i class="fa-brands fa-facebook"></i></a>
+                </div>
+
             </div>
 
-            <div className='col-md-4 text-center'>
-                <img src={E2} alt='m' />
+            <div className='col-md-4 text-center expert-image-right'>
+                <img src={E2} alt='m' className='e-image' />
                 <div className='expert-content'>
-                <h5>JOHN ABARAHAM</h5>
-                <p>WEB DEVELOPER</p>
+                <h5>GIMNA KATUGAMPALA</h5>
+                <p>SOFTWARE ENGINEER</p>
                 </div>
+
+                  {/* Social Media */}
+                  <div className='social-single-link'>
+                    <a href='https://twitter.com/kavishkagimna' target='_blank'><i class="fa-brands fa-twitter"></i></a>
+                    <a href='https://web.facebook.com/gimna.katugampala' target='_blank'><i class="fa-brands fa-facebook"></i></a>
+                </div>
+
             </div>
 
             <div className='col-md-4 text-center expert-image-right'>
                 <img src={B1} className='box-right' />
-                <img src={E3} alt='4' />
+                <img src={E3} alt='4' className='e-image' />
                 <div className='expert-content'>
-                <h5>MADAM QUERY</h5>
-                <p>WEB DESIGNER</p>
+                <h5>ABU BAKKAR</h5>
+                <p>UI/UX DESIGNER</p>
                 </div>
+
+                  {/* Social Media */}
+                  <div className='social-single-link'>
+                    <a href='#'><i class="fa-brands fa-twitter"></i></a>
+                    <a href='#'><i class="fa-brands fa-facebook"></i></a>
+                </div>
+
+            </div>
+
+             <div className='col-md-4 text-center expert-image-right'>
+                <img src={B1} className='box-right' />
+                <img src={E4} alt='4' className='e-image' />
+                <div className='expert-content'>
+                <h5>AYODEJI</h5>
+                <p>FLUTTER DEVELOPER</p>
+                </div>
+
+                {/* Social Media */}
+                <div className='social-single-link'>
+                    <a href='#'><i class="fa-brands fa-twitter"></i></a>
+                    <a href='#'><i class="fa-brands fa-facebook"></i></a>
+                </div>
+
             </div>
 
         </div>
