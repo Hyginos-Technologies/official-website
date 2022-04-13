5126a20ba42e41b34508b4631ca1d75732c158fe
gimnakatugampala <gimnakatugampala1@gmail.com>
2022-04-13 18:05:12 +0530

    navigation issue - responsiveness is solved

--- src/components/main-component/MainNav.js
+++ src/components/main-component/MainNav.js
@@ -2,17 +2,57 @@ import React from 'react'
 import {  Link } from "react-router-dom";
 import './MainNav.css'
 
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+import { FormControl } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import { NavDropdown } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Offcanvas } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+
 const MainNav = () => {
   return (
     <>
-    <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/about">About</Link></li>
-    <li><Link to="/services">Services</Link></li>
-    <li><Link to="/careers">Careers</Link></li>
-    <li><Link to="/portfolio">Portfolio</Link></li>
-    <li><Link to="/contact-us">Contact Us</Link></li>
-  </ul> 
+
+<Navbar collapseOnSelect expand="lg">
+  <Container fluid>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+      
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Hyginos Technologies</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/services">Services</Nav.Link>
+          <Nav.Link href="/careers">Careers</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+         
+        </Nav>
+
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
+
+ <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/careers">Careers</Link></li>
+          <li><Link to="/portfolio">Portfolio</Link></li>
+          <li><Link to="/contact-us">Contact Us</Link></li>
+        </ul> 
+
+    
     </>
   )
 }
